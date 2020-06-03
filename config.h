234fdd359903f57a759744044f34220c7d2d6c71
Richard Leek <richard@richardleek.com>
2020-06-03 10:42:16 -0400

    use pywal colors

--- config.h
+++ config.h
@@ -7,12 +7,14 @@ static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+//static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	/*[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-};
+};*/
+#include "/home/sorokya/.cache/wal/colors-wal-dmenu.h"
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
